60e767f66edac8f6c1e8052553ff9162536622f9
dinesh6714 <85017070+dinesh6714@users.noreply.github.com>
2021-07-02 12:12:58 +0530

    Update swap.c

--- swap.c
+++ swap.c
@@ -1,1 +1,25 @@
 
+#include <stdio.h>
+void swap_max(int s[], int l, int n ){
+    int large=s[n],nos,flag=0;
+  for(int i=n;i<l;i++){
+    if(s[i]>large){
+    flag=1;
+    large=s[i];
+     nos=i;
+      }
+      }
+      if(n!=l-1 && (flag==1)){
+      s[nos]=s[n];
+      s[n]=large;
+      }
+      for(int i=0;i<l;i++){
+    printf("%i ",s[i]);
+    }
+    printf("\n");
+         }
+         
+  void ssort(int s[], int l)  {
+  for(int i=0;i<l;i++)
+    swap_max(s,l,i);
+    }
